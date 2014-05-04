0c69229bb4b65898680b8d05ad70d8db458a8247
gureedo <gureedo@gmail.com>
2014-05-04 18:36:58 +0600

    added ButtonColumn

---
+++ src/ButtonColumn.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Intersvyaz\YayBootstrap;
+
+class ButtonColumn extends \CButtonColumn
+{
+	/**
+	 * The view button icon.
+	 * @var string
+	 */
+	public $viewButtonIcon = 'eye-open';
+
+	/**
+	 * The update button icon.
+	 * @var string
+	 */
+	public $updateButtonIcon = 'pencil';
+
+	/**
+	 * The delete button icon.
+	 * @var string
+	 */
+	public $deleteButtonIcon = 'trash';
+
+	/**
+	 * Whether to enable bootstrap tooltips on buttons.
+	 * @var bool
+	 */
+	public $enableTooltips = true;
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function initDefaultButtons()
+	{
+		parent::initDefaultButtons();
+
+		foreach (['view', 'update', 'delete'] as $id) {
+			if (!empty($this->{$id . 'ButtonIcon'}) && !isset($this->buttons[$id]['icon'])) {
+				$this->buttons[$id]['icon'] = $this->{$id . 'ButtonIcon'};
+			}
+		}
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	protected function renderButton($id, $button, $row, $data)
+	{
+		if (!empty($button['icon'])) {
+			if (!isset($button['options']['title']))
+				$button['options']['title'] = isset($button['label']) ? $button['label'] : $id;
+			$button['label'] = Html::icon($button['icon']);
+			$button['imageUrl'] = null;
+		}
+		if ($this->enableTooltips) {
+			$button['options']['data-toggle'] = 'tooltip';
+		}
+
+		parent::renderButton($id, $button, $row, $data);
+	}
+}
---
+++ tests/unit/ButtonColumnTest.php
@@ -0,0 +1,92 @@
+<?php
+
+use \Intersvyaz\YayBootstrap\ButtonColumn;
+
+/**
+ * @coversDefaultClass \Intersvyaz\YayBootstrap\ButtonColumn
+ */
+class ButtonColumnTest extends \PHPUnit_Framework_TestCase
+{
+	const WIDGET_CLASS = '\Intersvyaz\YayBootstrap\ButtonColumn';
+
+	/**
+	 * @return ButtonColumn
+	 */
+	private function makeWidget()
+	{
+		$grid = new CGridView();
+
+		return new ButtonColumn($grid);
+	}
+
+	protected function setUp()
+	{
+		Yii::import('zii.widgets.grid.CGridView');
+	}
+
+	/**
+	 * @coversNothing
+	 */
+	public function testInstanceAndAttributes()
+	{
+		$widget = $this->makeWidget();
+		$this->assertAttributeNotEmpty('viewButtonIcon', $widget);
+		$this->assertAttributeNotEmpty('updateButtonIcon', $widget);
+		$this->assertAttributeNotEmpty('deleteButtonIcon', $widget);
+		$this->assertAttributeInternalType('bool', 'enableTooltips', $widget);
+	}
+
+	/**
+	 * @covers ::initDefaultButtons
+	 */
+	public function testInitDefaultButtons()
+	{
+		$widget = $this->makeWidget();
+		$method = new ReflectionMethod(get_class($widget), 'initDefaultButtons');
+		$method->setAccessible(true);
+
+		$widget->viewButtonIcon = false;
+		$widget->buttons = ['delete' => ['icon' => 'foo']];
+		$method->invoke($widget);
+
+		$this->assertArrayNotHasKey('icon', $widget->buttons['view']);
+		$this->assertEquals($widget->updateButtonIcon, $widget->buttons['update']['icon']);
+		$this->assertEquals('foo', $widget->buttons['delete']['icon']);
+	}
+
+	/**
+	 * @covers ::renderButton
+	 */
+	public function testRenderButton()
+	{
+		$method = new ReflectionMethod(self::WIDGET_CLASS, 'renderButton');
+		$method->setAccessible(true);
+
+		ob_start();
+		$widget = $this->makeWidget();
+		$widget->enableTooltips = true;
+		$method->invokeArgs($widget, [
+			'view',
+			[
+				'icon' => 'foo',
+				'imageUrl' => 'http://blah'
+
+			],
+			'data',
+			'row'
+		]);
+		$widgetContent = ob_get_clean();
+		$this->assertTag([
+			'tag' => 'a',
+			'attributes' => [
+				'data-toggle' => 'tooltip',
+			],
+			'child' => [
+				'tag' => 'i',
+				'attributes' => [
+					'class' => 'regexp:/icon\-foo/'
+				]
+			]
+		], $widgetContent);
+	}
+}
